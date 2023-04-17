953ecf686d191880794283d2fda2a89b9c190ae8
Andrew Huang <ahuang@sdccd.edu>
2023-04-16 22:51:15 -0700

    Added data validation tests with examples for Car and Options

--- cisc179/Option.py
+++ cisc179/Option.py
@@ -1,6 +1,10 @@
 class Option:
     def __init__(self, details):
-        self.details = details
+        self.setDetails(details)
 
     def getDetails(self):
         return self.details
+
+    def setDetails(self, newDetails):
+        if newDetails == "" or len(newDetails) > 32 or not isinstance(newDetails, str):
+            raise ValueError("Option details must contain a string with less than 32 characters.")
--- tests/test_sport_utility_vehicle.py
+++
@@ -1,9 +0,0 @@
-from cisc179.SportUtilityVehicle import SportUtilityVehicle
-def testSportUtilityVehicle():
-   suv = SportUtilityVehicle("Toyota", 19000, 75995, 6, [], 4500);
-
-   assert suv.getManufacturerName() == "Toyota"
-   assert suv.getMilesOnVehicle() == 19000
-   assert suv.getPrice() == 75995
-   assert suv.getNumberOfSeats() == 6
-   assert suv.getMaxTowingWeight() == 4500
